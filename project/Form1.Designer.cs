399c0b53d046d7c112d6b4dc5525c15368980812
Shyzza <64161982+Shyzza@users.noreply.github.com>
2023-11-23 14:55:53 +0200

    Add project files.

---
+++ project/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(416, 270);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Location = new System.Drawing.Point(84, 73);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(242, 115);
+            this.panel2.TabIndex = 7;
+            this.panel2.Visible = false;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(89, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Restart";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(36, 19);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(177, 31);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Game Over !";
+            this.label7.Visible = false;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(310, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(157, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(183, 290);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "label6";
+            this.label6.Visible = false;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 285);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 24);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Score:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(375, 285);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "HP:";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 355);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Typing Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ project/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project
+{
+    public partial class Form1 : Form
+    {
+        Label[] L;
+        Random r = new Random();
+        const int N = 3;
+        int points = 0;
+        int hp = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+            updateLbl();
+        }
+
+        private void updateLbl() {
+
+            label6.Text = timer1.Interval.ToString();
+            label5.Text = "Score: " + points.ToString();
+            label4.Text = "HP: " + hp.ToString();
+        }
+
+        private void resetLbl(int i)
+        {
+            L[i].Top = 0;
+            L[i].Left = r.Next(0, panel1.Width - L[i].Width);
+            L[i].Text = Convert.ToString((char)r.Next(65, 90));
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            bool check = false;
+            for (int i = 0; i < N; i++)
+            {
+                if (L[i].Text == Convert.ToString(e.KeyCode))
+                {
+                    resetLbl(i);
+
+                    if (timer1.Interval > 25)
+                        timer1.Interval -= 25;
+                    else if (timer1.Interval > 1)
+                        timer1.Interval -= 1;
+
+                    points += 1;
+
+                    updateLbl();
+                    check = true;
+
+                }
+        
+            }
+            if (!check)
+            {
+                hp--;
+                    updateLbl();
+                if (hp < 1) Game_Over();
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            L = new Label[N];
+            L[0] = label1;
+            L[1] = label2;
+            L[2] = label3;
+
+
+            for (int i = 0; i < N; i++)
+            {
+                L[i].AutoSize = false;
+                L[i].BorderStyle = BorderStyle.FixedSingle;
+                L[i].TextAlign = ContentAlignment.MiddleCenter;
+                L[i].Width = 25;
+                L[i].Height = 25;
+                resetLbl(i);
+            }
+        }
+
+        private void Game_Over()
+        {
+
+            timer1.Stop();
+            label7.Visible = true;
+            panel2.Visible = true;
+            button1.Visible = true;
+            button1.Focus();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+
+            for (int i = 0; i < N; i++)
+            {
+                L[i].Top = L[i].Top + 2;
+                if (L[i].Top + L[i].Height >= panel1.Height)
+                {
+                    resetLbl(i);
+                    hp--;
+
+                    updateLbl();
+                    if (hp < 1) Game_Over();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // Reset game variables
+            points = 0;
+            hp = 5;
+            timer1.Interval = 200;
+
+            // Update UI elements
+            updateLbl();
+            label7.Visible = false;
+            panel2.Visible = false;
+            button1.Visible = false;
+
+            // Reset label positions and texts
+            for (int i = 0; i < N; i++)
+            {
+                resetLbl(i);
+            }
+
+            // Start the timer
+            timer1.Start();
+            this.Focus();
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+        private void label6_Click(object sender, EventArgs e)
+        {
+        }
+        private void label7_Click(object sender, EventArgs e)
+        {
+        }
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+        }
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+        }
+    }
+}
